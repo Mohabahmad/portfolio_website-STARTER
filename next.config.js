e94ff87cf653f176bbe769888143af0788bbadb5
mohabahmad <mohabmm666@gmail.com>
2025-03-29 11:44:18 +0530

    out

---
+++ next.config.js
@@ -0,0 +1,6 @@
+// next.config.js
+module.exports = {
+    output: 'export',
+    reactStrictMode: true, // Optional but a good practice for production
+    // Add any other configurations you might need
+  }
